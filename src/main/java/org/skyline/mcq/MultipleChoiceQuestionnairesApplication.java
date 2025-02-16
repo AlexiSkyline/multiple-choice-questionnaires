09146285f57d4e8856c4cbdecef5d32eda793a20
AlexiSkyline <ilegal_sprite@outlook.com>
2025-02-15 23:27:41 -0600

    dev: initial project

---
+++ src/main/java/org/skyline/mcq/MultipleChoiceQuestionnairesApplication.java
@@ -0,0 +1,13 @@
+package org.skyline.mcq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleChoiceQuestionnairesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleChoiceQuestionnairesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skyline/mcq/MultipleChoiceQuestionnairesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyline.mcq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleChoiceQuestionnairesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
