f0c7b5d366403336035fb4cc183d289ef814c52e
AlexiSkyline <ilegal_sprite@outlook.com>
2025-02-16 19:18:02 -0600

    feat: add entities and relationships for Answer and Result

---
+++ src/main/java/org/skyline/mcq/domain/Result.java
@@ -0,0 +1,52 @@
+package org.skyline.mcq.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+
+    @Id
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @GeneratedValue(generator = "UUID")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Survey survey;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp startTime;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp endTime;
+    private Integer duration;
+    private Integer totalPoints;
+    private Integer correctAnswers;
+    private Integer incorrectAnswers;
+
+    @OneToMany(mappedBy = "result")
+    private Set<Answer> answers;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+}
---
+++ src/main/resources/db/migration/V3__create-result-and-answer-tables.sql
@@ -0,0 +1,33 @@
+drop table if exists result;
+drop table if exists answer;
+
+create table result (
+    id varchar(36) NOT NULL,
+	user_id varchar(36) NOT NULL,
+	survey_id varchar(36) NOT NULL,
+	start_time datetime(6),
+	end_time datetime(6),
+	duration BIGINT,
+	total_points int,
+	correct_answers int,
+	incorrect_answers int,
+    created_at datetime(6),
+    primary key (id),
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT FOREIGN KEY (survey_id) REFERENCES survey (id)
+) engine=InnoDB;
+
+create table answer (
+    id varchar(36) NOT NULL,
+    user_id varchar(36) NOT NULL,
+    question_id varchar(36) NOT NULL,
+    result_id varchar(36) NOT NULL,
+    user_answers text,
+    is_correct tinyint(1),
+    points int,
+    created_at datetime(6),
+    primary key (id),
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT FOREIGN KEY (question_id) REFERENCES question (id),
+    CONSTRAINT FOREIGN KEY (result_id) REFERENCES result (id)
+) engine=InnoDB;
