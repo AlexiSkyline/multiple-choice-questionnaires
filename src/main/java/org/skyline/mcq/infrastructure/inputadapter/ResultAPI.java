984da18faea8f6b388d716945291a0d583feb4b6
AlexiSkyline <ilegal_sprite@outlook.com>
2025-04-10 23:52:09 -0600

    feat: restrict updates to Answer owner only and enhance security in ResultAPI

--- src/main/java/org/skyline/mcq/infrastructure/inputadapter/ResultAPI.java
+++ src/main/java/org/skyline/mcq/infrastructure/inputadapter/ResultAPI.java
@@ -6,12 +6,14 @@ import org.skyline.mcq.application.dtos.output.ResultResponseDto;
 import org.skyline.mcq.domain.exceptions.NotFoundException;
 import org.skyline.mcq.infrastructure.http.ResponseHandler;
 import org.skyline.mcq.infrastructure.inputport.AccountInputPort;
+import org.skyline.mcq.infrastructure.inputport.JwtInputPort;
 import org.skyline.mcq.infrastructure.inputport.ResultInputPort;
 import org.skyline.mcq.infrastructure.http.dto.ResponseBody;
 import org.skyline.mcq.infrastructure.inputport.SurveyInputPort;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,31 +31,36 @@ public class ResultAPI {
     private final SurveyInputPort surveyInputPort;
     private final AccountInputPort accountInputPort;
     private final ResultInputPort resultInputPort;
+    private final JwtInputPort jwtInputPort;
     private final ResponseHandler responseHandler;
 
     @GetMapping(RESULT_PATH_ID)
+    @PreAuthorize("hasRole('SURVEY_RESPONDENT')")
     public ResponseEntity<ResponseBody<ResultResponseDto>> getResultById(@PathVariable UUID resultId) {
-        return resultInputPort.findResultById(resultId).map(result -> responseHandler.responseBuild(
-                HttpStatus.OK,
-                "Requested Result is given here",
-                result
-        )).orElseThrow(() -> new NotFoundException(
-                "Result",
-                resultId.toString(),
-                "Please provide a valid result ID"
-        ));
+        UUID accountId = jwtInputPort.getCurrentUserDetails().getId();
+        validateAccountExists(accountId);
+
+        return resultInputPort.findResultById(resultId, accountId)
+                .filter(resultResponseDto ->
+                    resultResponseDto.getAccount().getId().equals(accountId)
+                ).map(result -> responseHandler.responseBuild(
+                        HttpStatus.OK,
+                        "Requested Result is given here",
+                        result
+                )).orElseThrow(() -> new NotFoundException("Result", resultId.toString(), "Please provide a valid result ID"));
     }
 
     @GetMapping(RESULT_PATH + "/survey/{surveyId}")
+    @PreAuthorize("hasRole('ROLE_SURVEY_CREATOR')")
     public ResponseEntity<ResponseBody<Page<ResultResponseDto>>> getResultBySurveyId(@PathVariable UUID surveyId,
                                                                                      @RequestParam(required = false) @Positive Integer pageNumber,
                                                                                      @RequestParam(required = false) @Positive Integer pageSize) {
 
-        surveyInputPort.findSurveyById(surveyId).orElseThrow(() -> new NotFoundException(
-                "Survey",
-                surveyId.toString(),
-                "Please provide a valid survey ID"
-        ));
+        surveyInputPort.findSurveyById(surveyId)
+                .filter(surveyResponseDto ->
+                        surveyResponseDto.getAccount().getId().equals(jwtInputPort.getCurrentUserDetails().getId())
+                )
+                .orElseThrow(() -> new NotFoundException("Survey", surveyId.toString(), "Please provide a valid survey ID"));
 
         return responseHandler.responseBuild(
                 HttpStatus.OK,
@@ -62,16 +69,13 @@ public class ResultAPI {
         );
     }
 
-    @GetMapping(RESULT_PATH + "/account/{accountId}")
-    public ResponseEntity<ResponseBody<Page<ResultResponseDto>>> getResultByAccountId(@PathVariable UUID accountId,
-                                                                                      @RequestParam(required = false) @Positive Integer pageNumber,
+    @GetMapping(RESULT_PATH + "/account")
+    @PreAuthorize("hasRole('SURVEY_RESPONDENT')")
+    public ResponseEntity<ResponseBody<Page<ResultResponseDto>>> getAllResultByAccountId(@RequestParam(required = false) @Positive Integer pageNumber,
                                                                                       @RequestParam(required = false) @Positive Integer pageSize) {
 
-        accountInputPort.getAccountById(accountId).orElseThrow(() -> new NotFoundException(
-                "Account",
-                accountId.toString(),
-                "Please provide a valid account ID"
-        ));
+        UUID accountId = jwtInputPort.getCurrentUserDetails().getId();
+        validateAccountExists(accountId);
 
         return responseHandler.responseBuild(
                 HttpStatus.OK,
@@ -81,26 +85,28 @@ public class ResultAPI {
     }
 
     @GetMapping(RESULT_PATH + "/survey/{surveyId}/account/{accountId}")
+    @PreAuthorize("hasRole('SURVEY_RESPONDENT')")
     public ResponseEntity<ResponseBody<Page<ResultResponseDto>>> getResultBySurveyIdAndAccountId(@PathVariable UUID surveyId, @PathVariable UUID accountId,
                                                                                                  @RequestParam(required = false) @Positive Integer pageNumber,
                                                                                                  @RequestParam(required = false) @Positive Integer pageSize) {
 
-        surveyInputPort.findSurveyById(surveyId).orElseThrow(() -> new NotFoundException(
-                "Survey",
-                surveyId.toString(),
-                "Please provide a valid survey ID"
-        ));
+        validateAccountExists(accountId);
 
-        accountInputPort.getAccountById(accountId).orElseThrow(() -> new NotFoundException(
-                "Account",
-                accountId.toString(),
-                "Please provide a valid account ID"
-        ));
+        surveyInputPort.findSurveyById(surveyId)
+                .orElseThrow(() -> new NotFoundException("Survey", surveyId.toString(), "Please provide a valid survey ID"));
 
         return responseHandler.responseBuild(
                 HttpStatus.OK,
                 "Requested Result is given here",
                 resultInputPort.listResultBySurveyIdAndAccountId(surveyId, accountId, pageNumber, pageSize)
         );
+    }
+
+    public void validateAccountExists(UUID accountId) {
+        accountInputPort.getAccountById(accountId).orElseThrow(() -> new NotFoundException(
+                "Account",
+                accountId.toString(),
+                "Please provide a valid account ID"
+        ));
     }
 }
--- src/test/java/org/skyline/mcq/infrastructure/inputadapter/ResultAPITest.java
+++ src/test/java/org/skyline/mcq/infrastructure/inputadapter/ResultAPITest.java
@@ -1,178 +1,229 @@
 package org.skyline.mcq.infrastructure.inputadapter;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.skyline.mcq.application.dtos.input.SignUpRequestDto;
+import org.skyline.mcq.domain.enums.TypeRole;
+import org.skyline.mcq.domain.exceptions.NotFoundException;
 import org.skyline.mcq.domain.models.Account;
+import org.skyline.mcq.domain.models.Category;
 import org.skyline.mcq.domain.models.Result;
 import org.skyline.mcq.domain.models.Survey;
 import org.skyline.mcq.infrastructure.outputport.AccountRepository;
+import org.skyline.mcq.infrastructure.outputport.CategoryRepository;
 import org.skyline.mcq.infrastructure.outputport.ResultRepository;
 import org.skyline.mcq.infrastructure.outputport.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ResultAPITest {
 
     private static final String RESULT_PATH = "/api/v1/results";
-    private static final String RESULT_PATH_ID = "/api/v1/results/{resultId}";
+    private static final String RESULT_PATH_ID = RESULT_PATH + "/{resultId}";
     private static final String RESULT_PATH_SURVEY_ID = RESULT_PATH + "/survey/{surveyId}";
-    private static final String RESULT_PATH_ACCOUNT_ID = RESULT_PATH + "/account/{accountId}";
-    private static final String RESULT_PATH_SURVEY_ID_ACCOUNT_ID = RESULT_PATH + "/survey/{surveyId}/account/{accountId}";
+    private static final String RESULT_PATH_ACCOUNT_ID = RESULT_PATH_SURVEY_ID + "/account/{accountId}";
+    private static final String RESULT_PATH_MY_ACCOUNT = RESULT_PATH + "/account";
 
     @Autowired
-    private ResultRepository resultRepository;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    private SurveyRepository surveyRepository;
+    private ResultAPI resultAPI;
 
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
-    private WebApplicationContext wac;
-
     private MockMvc mockMvc;
-    private Result resultTest;
-    private Survey surveyTest;
-    private Account accountTest;
 
-    @BeforeEach
-    void setUp() {
+    private static String tokenRespondent;
+    private static String tokenCreator;
+    private static UUID surveyId;
+    private static UUID resultId;
+    private static UUID accountId;
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-        resultTest = Result.builder()
-                .startTime(Timestamp.valueOf(LocalDateTime.now()))
-                .endTime(Timestamp.valueOf(LocalDateTime.now().plusMinutes(30)))
-                .duration(1800)
-                .totalPoints(20)
-                .correctAnswers(10)
-                .incorrectAnswers(10)
-                .build();
-        surveyTest = Survey.builder()
-                .title("new title")
-                .description("new description")
-                .image("survey.png")
+    @BeforeAll
+    static void initializeTestEnvironment(
+            @Autowired AuthAPI authAPI,
+            @Autowired AccountRepository accountRepository,
+            @Autowired ResultRepository resultRepository,
+            @Autowired CategoryRepository categoryRepository,
+            @Autowired SurveyRepository surveyRepository
+    ) {
+        var testRespondent = buildSignUpRequest("respondentResult", "lastname admin", "respondent.result.test@gmail.com");
+        var testCreator = buildSignUpRequest("creatorResult", "lastname creator", "creator.result.test@gmail.com");
+
+        tokenRespondent = registerUserAndGetToken(authAPI, testRespondent, TypeRole.ROLE_SURVEY_RESPONDENT);
+        tokenCreator = registerUserAndGetToken(authAPI, testCreator, TypeRole.ROLE_SURVEY_CREATOR);
+
+        var category = categoryRepository.findAll().getFirst();
+
+        var creator = accountRepository.findByEmail(testCreator.getEmail()).orElseThrow();
+        var survey = createTestSurvey(surveyRepository, creator, category);
+        surveyId = survey.getId();
+
+        var respondent = accountRepository.findByEmail(testRespondent.getEmail()).orElseThrow();
+        accountId = respondent.getId();
+        resultId = createTestResult(resultRepository, respondent, survey);
+    }
+
+    private static Survey createTestSurvey(SurveyRepository repository, Account account, Category category) {
+        return repository.save(Survey.builder()
+                .title("test title")
+                .description("test description")
+                .image("test image")
                 .maxPoints(10)
                 .questionCount(5)
-                .active(true)
                 .timeLimit(3600)
                 .attempts(1)
                 .hasRestrictedAccess(true)
+                .category(category)
+                .account(account)
+                .status(true)
+                .build());
+    }
+
+    private static UUID createTestResult(ResultRepository repository, Account account, Survey survey) {
+        Result result = Result.builder()
+                .account(account)
+                .survey(survey)
+                .startTime(Timestamp.valueOf(LocalDateTime.now()))
+                .endTime(Timestamp.valueOf(LocalDateTime.now().plusMinutes(30)))
+                .duration(1800)
+                .totalPoints(10)
+                .correctAnswers(5)
+                .incorrectAnswers(0)
                 .build();
-        accountTest = Account.builder()
-                .firstName("new account")
-                .lastName("new account")
-                .username("new_ac_responder")
-                .email("new.account@example.com")
-                .password("NewPassword123")
-                .profileImage("new.account1.jpg")
-                .description("New Account responder")
+
+        return repository.save(result).getId();
+    }
+
+    private static SignUpRequestDto buildSignUpRequest(String firstName, String lastName, String email) {
+        return SignUpRequestDto.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .username(firstName)
+                .email(email)
+                .password("password")
                 .build();
+    }
+
+    private static String registerUserAndGetToken(AuthAPI authAPI, SignUpRequestDto request, TypeRole role) {
+        return Objects.requireNonNull(authAPI.registerUserWithRole(request, role).getBody()).getAccessToken();
+    }
+
+    private ResultActions performAuthorizedRequest(HttpMethod method, String path, String token, Object body, Object... uriVars) throws Exception {
+        var request = MockMvcRequestBuilders.request(method, path, uriVars)
+                .header("Authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        if (body != null) {
+            request.content(objectMapper.writeValueAsString(body));
+        }
+
+        return mockMvc.perform(request);
     }
 
     @Test
     @Rollback
     @Transactional
-    @DisplayName("Get Result by ID: Should return 200 OK")
+    @DisplayName("(RESPONDENT) Get Result by ID: Should return 200 OK")
     void testGetResultById() throws Exception {
-
-        var result = resultRepository.save(resultTest);
-        mockMvc.perform(get(RESULT_PATH_ID, result.getId()))
-                .andExpect(status().isOk());
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_ID, tokenRespondent, null, resultId)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.data").isNotEmpty()
+                );
     }
 
     @Test
-    @DisplayName("Get Result by ID: Should return 404 Not Found")
+    @DisplayName("(RESPONDENT) Get Result by ID: Should return 404 Not Found")
     void testGetResultByIdNotFound() throws Exception {
-
-        mockMvc.perform(get(RESULT_PATH_ID, UUID.randomUUID()))
-                .andExpect(status().isNotFound());
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_ID, tokenRespondent, null, UUID.randomUUID())
+                .andExpectAll(
+                        status().isNotFound(),
+                        jsonPath("$.status").value(404)
+                );
     }
 
     @Test
-    @Rollback
-    @Transactional
-    @DisplayName("Get Result by Survey ID: Should return 200 OK")
+    @DisplayName("(CREATOR) Get Results by Survey ID: Should return 200 OK")
     void testGetResultBySurveyId() throws Exception {
-
-        var result = surveyRepository.save(surveyTest);
-        mockMvc.perform(get(RESULT_PATH_SURVEY_ID, result.getId()))
-                .andExpect(status().isOk());
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_SURVEY_ID, tokenCreator, null, surveyId)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.data.content").isArray()
+                );
     }
 
     @Test
-    @DisplayName("Get Result by Survey ID: Should return 404 Not Found")
+    @DisplayName("(CREATOR) Get Results by Survey ID: Should return 404 Not Found")
     void testGetResultBySurveyIdNotFound() throws Exception {
-
-        mockMvc.perform(get(RESULT_PATH_SURVEY_ID, UUID.randomUUID()))
-                .andExpect(status().isNotFound());
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_SURVEY_ID, tokenCreator, null, UUID.randomUUID())
+                .andExpectAll(
+                        status().isNotFound(),
+                        jsonPath("$.status").value(404)
+                );
     }
 
     @Test
-    @Rollback
-    @Transactional
-    @DisplayName("Get Result by Account ID: Should return 200 OK")
-    void testGetResultByAccountId() throws Exception {
-
-        var result = accountRepository.save(accountTest);
-        mockMvc.perform(get(RESULT_PATH_ACCOUNT_ID, result.getId()))
-                .andExpect(status().isOk());
+    @DisplayName("(RESPONDENT) Get all result of my account: Should return 200 OK")
+    void testGetAllResultOfAccount() throws Exception {
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_MY_ACCOUNT, tokenRespondent, null)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.data.content").isArray()
+                );
     }
 
     @Test
-    @DisplayName("Get Result by Account ID: Should return 404 Not Found")
-    void testGetResultByAccountIdNotFound() throws Exception {
-
-        mockMvc.perform(get(RESULT_PATH_ACCOUNT_ID, UUID.randomUUID()))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @Rollback
-    @Transactional
-    @DisplayName("Get Result by Survey ID and Account ID: Should return 200 OK")
+    @DisplayName("(RESPONDENT) Get Result by Survey Id and Account Id: Should return 200 OK")
     void testGetResultBySurveyIdAndAccountId() throws Exception {
-
-        var survey = surveyRepository.save(surveyTest);
-        var result = accountRepository.save(accountTest);
-
-        mockMvc.perform(get(RESULT_PATH_SURVEY_ID_ACCOUNT_ID, survey.getId(), result.getId()))
-                .andExpect(status().isOk());
+        performAuthorizedRequest(HttpMethod.GET, RESULT_PATH_ACCOUNT_ID, tokenRespondent, null, surveyId, accountId)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.data.content").isArray()
+                );
     }
 
     @Test
-    @Rollback
-    @Transactional
-    @DisplayName("Get Result by Survey ID and Account ID: Should return 404 Not Found when survey does not exist")
-    void testGetResultBySurveyIdAndAccountIdNotFoundSurvey() throws Exception {
-
-        var result = accountRepository.save(accountTest);
-        mockMvc.perform(get(RESULT_PATH_SURVEY_ID_ACCOUNT_ID, UUID.randomUUID(), result.getId()))
-                .andExpect(status().isNotFound());
+    @DisplayName("Validate account exists: should not throw exception")
+    void testValidateAccountExists() {
+        assertDoesNotThrow(() -> resultAPI.validateAccountExists(accountId));
     }
 
     @Test
-    @Rollback
-    @Transactional
-    @DisplayName("Get Result by Survey ID and Account ID: Should return 404 Not Found when account does not exist")
-    void testGetResultBySurveyIdAndAccountIdNotFoundAccount() throws Exception {
+    @DisplayName("Validate account does not exist: should throw NotFoundException")
+    void validateAccountNotExist() {
+        UUID invalidAccountId = UUID.randomUUID();
 
-        var survey = surveyRepository.save(surveyTest);
-        mockMvc.perform(get(RESULT_PATH_SURVEY_ID_ACCOUNT_ID, survey.getId(), UUID.randomUUID()))
-                .andExpect(status().isNotFound());
+        NotFoundException ex = assertThrows(NotFoundException.class, () ->
+                resultAPI.validateAccountExists(invalidAccountId)
+        );
+
+        assertAll(
+                () -> assertFalse(ex.getMessage().isEmpty()),
+                () -> assertTrue(ex.getMessage().contains(invalidAccountId.toString())),
+                () -> assertEquals("Account with identifier '" + invalidAccountId + "' was not found. Please provide a valid account ID", ex.getMessage())
+        );
     }
 }
